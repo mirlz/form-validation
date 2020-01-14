83a9e2f91de06040dfffd5d7e44cb5ce4bd63e95
eugenia.poh21 <eugenia.poh@titansoft.com.sg>
2020-01-14 12:53:11 +0800

    set up css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import RegistrationPage from './views/RegistrationPage';
-import './App.css';
+import './sass/custom.min.css';
 
 function App() {
   return (
