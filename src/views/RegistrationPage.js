53cd0e1d5107f18d83fcd191016a13bc97b6b91f
eugenia.poh21 <eugenia.poh@titansoft.com.sg>
2020-01-14 12:49:19 +0800

    remove mobx-router, install react-router instead set up basic router for homepage

---
+++ src/views/RegistrationPage.js
@@ -0,0 +1,10 @@
+import React from 'react';   
+import {observer} from 'mobx-react';
+
+const RegistrationPage = observer((props) => {
+    return (
+        <div>Registration Page</div>
+    )
+})
+
+export default RegistrationPage;
